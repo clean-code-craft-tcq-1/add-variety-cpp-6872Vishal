ed0552c74498191db8f3e13fa2caafbdb2d15d3c
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-03-31 17:59:49 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,3 +6,33 @@
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
+
+TEST_CASE("classifies the breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 36) == TOO_HIGH);
+}
+
+TEST_CASE("classifies the breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
+}
+
+TEST_CASE("classifies the breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 56) == TOO_HIGH);
+}
+
+TEST_CASE("classifies the breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -2.5) == TOO_LOW);
+}
+
+TEST_CASE("classifies the breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 41) == TOO_HIGH);
+}
+
+TEST_CASE("classifies the breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -5.5) == TOO_LOW);
+}
+
+// checkAndAlert
+// TEST_CASE("Check and Alert Test Case") {
+// BatteryCharacter temp_st = {PASSIVE_COOLING, "BOSCH" };
+//   REQUIRE(checkAndAlert(TO_EMAIL,temp_st,22));
+// }
